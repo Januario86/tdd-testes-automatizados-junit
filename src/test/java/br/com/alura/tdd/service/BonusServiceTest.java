6068cabf5f230efa0e89772ae4a957e5262139f0
Januario86 <januario86fp@gmail.com>
2021-11-02 06:59:51 -0300

    fix:first refactory BonusServiceTest

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,6 @@
 package br.com.alura.tdd.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -15,17 +15,9 @@ public class BonusServiceTest {
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Felipe",LocalDate.now(), new BigDecimal("25000")));
 		
-		assertEquals(new BigDecimal("0.00"), bonus);
-	}
-	
-	@Test
-	void bonusDeveriaSer10PorCentoDoSalario() {
-		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Felipe",LocalDate.now(), new BigDecimal("25000")));
-		
-		assertEquals(new BigDecimal("250.00"), bonus);
+		assertThrows(IllegalArgumentException.class,
+				() -> service.calcularBonus(new Funcionario("Felipe",LocalDate.now(), new BigDecimal("25000"))));		
 	}
 	
 }
